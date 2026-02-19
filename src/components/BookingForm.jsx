38153579d880c32dc1bf3c288d94f52ddc358bde
Carina <crowliflower818@gmail.com>
2026-02-19 17:00:25 -0500

    Daily work and bug fixes. Fixed some weird styling choices, starting work on building content for the other pages.

---
+++ src/components/BookingForm.jsx
@@ -0,0 +1,9 @@
+import styles from "./bookingForm.module.css";
+
+function BookingForm() {
+    return (
+        <div className={styles.formContainer}></div>
+    )
+}
+
+export default BookingForm;
