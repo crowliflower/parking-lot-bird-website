7dfe6ad82eb6b9280c7a2e7136507b71fadaf6ce
Carina <crowliflower818@gmail.com>
2026-03-15 15:49:41 -0400

    Bringing in Tailwindcss and DaisyUI to help with development time. Still need to properly integrate these

--- src/components/PhotoBar.jsx
+++ src/components/PhotoBar.jsx
@@ -5,14 +5,14 @@ function PhotoBar() {
     <>
       <div className={styles.mainContainer}>
         <ul className={styles.thumbnailBar}>
-          <li className={styles.photoThumbnail}></li>
-          <li className={styles.photoThumbnail}></li>
-          <li className={styles.photoThumbnail}></li>
-          <li className={styles.photoThumbnail}></li>
-          <li className={styles.photoThumbnail}></li>
-          <li className={styles.photoThumbnail}></li>
-          <li className={styles.photoThumbnail}></li>
-          <li className={styles.photoThumbnail}></li>
+          <li className={styles.photoThumbnail}><img src='src/assets/test_pic_2.jpg' alt="" /></li>
+          <li className={styles.photoThumbnail}><img src='src/assets/test_pic_3.jpg' alt="" /></li>
+          <li className={styles.photoThumbnail}><img src='src/assets/test_pic_4.jpg' alt="" /></li>
+          <li className={styles.photoThumbnail}><img src='src/assets/test_pic_5.jpg' alt="" /></li>
+          <li className={styles.photoThumbnail}><img src='src/assets/test_pic_1.jpg' alt="" /></li>
+          <li className={styles.photoThumbnail}><img src='src/assets/test_pic_2.jpg' alt="" /></li>
+          <li className={styles.photoThumbnail}><img src='src/assets/test_pic_3.jpg' alt="" /></li>
+          <li className={styles.photoThumbnail}><img src='src/assets/test_pic_4.jpg' alt="" /></li>
         </ul>
       </div>
     </>
